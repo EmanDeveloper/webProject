a6cd8c5fd84a61f6c9b342fac61316bcf2b813b9
EmanDeveloper <memankhadim@gmail.com>
2024-12-17 18:43:50 +0500

    first change

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,15 +9,18 @@
                 <a class="nav-link hover-effect" href="{{ route('home') }}">Home</a>
             </div>
             <div class="navbar-nav ms-auto">
+                
                 @auth
+                <a class="nav-link air-bnb" href="{{ route('listings.create') }}">Airbnb your home</a>
                     <!-- If user is logged in, show Login button -->
                     <a class="nav-link btn edit-btn" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><b>Logout</b></a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
+
                 @else
                     <!-- If user is not logged in, show Signup button -->
-                    <a class="nav-link air-bnb" href="{{ route('signup') }}">Airbnb your home</a>
+                    
                     <a class="nav-link active edit" href="{{ route('signup') }}"><b>Signup</b></a>
                     <a class="nav-link btn edit-btn" href="{{ route('login') }}"><b>Login</b></a>
                 @endauth
